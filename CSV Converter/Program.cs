c45b0f436035564a65a182806ea48c4cb91ee694
montse-villa03 <montserratvilla0113@gmail.com>
2024-07-12 11:03:01 -0600

    new function to Json

--- CSV Converter/Program.cs
+++ CSV Converter/Program.cs
@@ -55,11 +55,8 @@ namespace CSV_Converter
                     //Convertir datos de CSV a JSON
                     case "1":
                         //Convertir datos de CSV a JSON
-                        JSONConverter JSONConv = new JSONConverter(csvData);
-                        var json = JSONConv.ConvertToJson();
-                        Logger.WriteLog("Writing JSON file...");
-                        File.WriteAllText(jsonPath, json);
-                        Logger.WriteLog("JSON file has been written successfully.");
+                        var json = JSONConverter.ConvertToJson(csvData);
+                        JSONConverter.WriteFile(jsonPath, json);
                         break;
 
                     //Convertir datos de CSV a XML
